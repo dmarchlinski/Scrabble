f3ceafb66994487fecae039302bff962dc33e626
tatep <33260588+tatep@users.noreply.github.com>
2018-05-11 15:14:28 -0400

    Rename board.py to board.1.py

---
+++ ui/board.1.py
@@ -0,0 +1,9 @@
+def main():
+    board = []
+    for i in range(10):
+        board.append(["[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]",])
+
+    for i in board:
+        print i[0]+i[1]+i[2]+i[3]+i[4]+i[5]+i[6]+i[7]+i[8]+i[9]+"\n"
+
+main()
