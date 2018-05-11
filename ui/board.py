529b5d2802b447455df7cd2ec9ef39dac0494440
tatep <33260588+tatep@users.noreply.github.com>
2018-05-11 15:14:54 -0400

    Update board.py

--- ui/board.py
+++ ui/board.py
@@ -4,6 +4,6 @@ def main():
         board.append(["[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]","[\t]",])
 
     for i in board:
-        print i[0]+i[1]+i[2]+i[3]+i[4]+i[5]+i[6]+i[7]+i[8]+i[9]
+        print i[0]+i[1]+i[2]+i[3]+i[4]+i[5]+i[6]+i[7]+i[8]+i[9]+"\n"
 
 main()
