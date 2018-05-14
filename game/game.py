4e93e3132a26f4822e469a3c4a94ec943d0d1b56
Dylan Marchlinski <dylanmarchlinski@Dylans-MacBook-Air.local>
2018-05-14 13:20:36 -0400

    fixes

--- game/game.py
+++ game/game.py
@@ -26,21 +26,25 @@ def main():
         chosen.append(consonants[randint(0, 20)])
     print "Your chosen letters are " + str(",".join(chosen))
     valid = True
+    valid
     index = 0
     words = open("words.txt", "r").readlines()
     while valid:
         guess = raw_input("Create a word with your chosen letters: ")
+        valid2 = True
         for i in guess:
-            if i not in chosen and guess + "\n" in words:
-                print "That is not a valid guess. Enter something only uses your chosen letters and is a word."
-                valid = True
-            else:
-                if i == max(guess):
-                    print "That is a valid word!"
-                    print "The score for that word is", scrabble_score(guess)
-                valid = False
+            while valid2:
+                if i not in chosen:
+                    print "That is not a valid guess. Enter something only uses your chosen letters and is a word."
+                    valid = True
+                    valid2 = False
+                elif guess + "\n" in words:
+                    if i == max(guess):
+                        print "That is a valid word!"
+                        print "The score for that word is", scrabble_score(guess)
+                        valid = False
+                        valid2 = False
         index += 1
-    print words[20]
 
 
 main()
