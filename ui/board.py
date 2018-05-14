a4c0edcc3f11f827fab89d3d7530d42fb5ed5409
Tate Park <tatepark@Tates-MacBook-Pro.local>
2018-05-14 13:31:09 -0400

    board

--- ui/board.py
+++ ui/board.py
@@ -1,10 +1,15 @@
 def main():
-    board = []
-    for i in range(10):
-        board.append(["[\t]", "[\t]", "[\t]", "[\t]", "[\t]", "[\t]", "[\t]", "[\t]", "[\t]", "[\t]"])
 
-    for i in board:
-        print i[0]+i[1]+i[2]+i[3]+i[4]+i[5]+i[6]+i[7]+i[8]+i[9]+"\n"
+    board_letters = [["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"],["a","b","c","d","e","f","g","h","i","j"]]
+    boardui = []
+    for i in range(10):
+        boardui.append([])
+        for j in range(10):
+            boardui[i].append("[ "+str(board_letters[i][j])+" ]")
+    for i in boardui:
+        print i[0]+i[1]+i[2]+i[3]+i[4]+i[5]+i[6]+i[7]+i[8]+"\n"
+
+    del boardui[:]
 
 
 main()
